454755b658abec57cca206be102a6d44a715d2a4
PabloMK7 <hackyglitch2@gmail.com>
2023-11-14 19:48:33 +0100

    Implement some missing/wrong AC functionality.

--- src/core/hle/service/nwm/nwm_uds.cpp
+++ src/core/hle/service/nwm/nwm_uds.cpp
@@ -575,6 +575,10 @@ void NWM_UDS::Shutdown(Kernel::HLERequestContext& ctx) {
 
     recv_buffer_memory.reset();
 
+    SharedPage::Handler& shared_page = Core::System::GetInstance().Kernel().GetSharedPageHandler();
+    shared_page.SetWifiLinkLevel(SharedPage::WifiLinkLevel::OFF);
+    shared_page.SetWifiState(SharedPage::WifiState::ENABLED);
+
     IPC::RequestBuilder rb = rp.MakeBuilder(1, 0);
     rb.Push(RESULT_SUCCESS);
     LOG_DEBUG(Service_NWM, "called");
@@ -666,6 +670,10 @@ ResultVal<std::shared_ptr<Kernel::Event>> NWM_UDS::Initialize(
         node_info.push_back(current_node);
         channel_data.clear();
     }
+
+    SharedPage::Handler& shared_page = Core::System::GetInstance().Kernel().GetSharedPageHandler();
+    shared_page.SetWifiLinkLevel(SharedPage::WifiLinkLevel::BEST);
+    shared_page.SetWifiState(SharedPage::WifiState::LOCAL1);
 
     return connection_status_event;
 }
@@ -1506,7 +1514,6 @@ NWM_UDS::NWM_UDS(Core::System& system) : ServiceFramework("nwm::UDS"), system(sy
     }
 
     system.Kernel().GetSharedPageHandler().SetMacAddress(mac);
-    system.Kernel().GetSharedPageHandler().SetWifiLinkLevel(SharedPage::WifiLinkLevel::BEST);
 
     if (auto room_member = Network::GetRoomMember().lock()) {
         wifi_packet_received = room_member->BindOnWifiPacketReceived(
